2679ccad7aeb93a81b0f2cbb431d28d0f89de8bc
j2hxxx <77jihyun77@naver.com>
2023-09-12 17:58:53 +0900

    feat: TableHead 추가

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,12 +1,46 @@
-import { Container } from "@mui/material"
+import { useEffect } from "react";
+import {
+  Container,
+  CssBaseline,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
+} from "@mui/material"
 
 
 const History = () => {
+  return (
+    <Container component="main" maxWidth="xl">
+      <CssBaseline />
+      <Typography component="h1" variant="h5" sx={{py: 3}}>
+        이력 확인
+      </Typography>
+      
+      <TableContainer component={Paper}>
+        <Table aria-label="Usage History">
+          <TableHead>
+            <TableRow>
+              <TableCell sx = {{minWidth:50}} align="center"> username </TableCell>
+              <TableCell sx = {{minWidth:400}} align="center">출발지</TableCell>
+              <TableCell sx = {{minWidth:100}} align="center">출발시간</TableCell>
+              <TableCell sx = {{minWidth:400}} align="center">도착지</TableCell>
+              <TableCell sx = {{minWidth:100}} align="center">도착시간 </TableCell>
+              <TableCell sx = {{minWidth:100}} align="center">가격&nbsp;(원)</TableCell>
+            </TableRow> 
+          </TableHead>
 
-
-    return (
-        <Container> Hello world!</Container>
-    )
+          <TableBody>
+            
+          </TableBody>
+        </Table>
+      </TableContainer> 
+    </Container>
+  )
 };
 
 export default History;
