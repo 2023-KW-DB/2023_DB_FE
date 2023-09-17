bfcfd007aa9177a29833543248171df018e6f420
j2hxxx <77jihyun77@naver.com>
2023-09-17 16:14:53 +0900

    feat: Payment

---
+++ src/pages/Payment.js
@@ -0,0 +1,14 @@
+import { Container, Button } from "@mui/material"
+import { useEffect } from "react";
+
+const Payment = () => {
+  return(
+    <Container>
+      <Button variant="contained" color="primary">
+        payment.
+      </Button> 
+    </Container>     
+  )
+};
+
+export default Payment;
