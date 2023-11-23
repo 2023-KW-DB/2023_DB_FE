b976e0c9e7ad5185770424b4331c3dfbed634527
김성진 <mirusu400@naver.com>
2023-11-23 11:18:03 +0900

    feat: Admin 패널 User 데이터 API 연결

--- src/pages/admin/AdminUser.js
+++ src/pages/admin/AdminUser.js
@@ -20,111 +20,105 @@ import moment from "moment";
 
 const mock = [
   {
-    id: 1,
-    userid: "testuser1",
-    username: "테스트유저1",
-    usertype: "0",
-    email: "testuser1@naver.com",
-    phone_number: "010-1234-5678",
-    weight: "70",
-    age: "23",
-    last_accessed_at: "2021-10-01T14:46:00.000Z",
-    total_money: "100000",
-  },
-  {
-    id: 2,
-    userid: "testuser2",
-    username: "테스트유저2",
-    usertype: "0",
-    email: "testuser2@naver.com",
-    phone_number: "010-1234-5678",
-    weight: "70",
-    age: "23",
-    last_accessed_at: "2021-10-01T14:46:00.000Z",
-    total_money: "100000",
-  },
-  {
-    id: 3,
-    userid: "testuser3",
-    username: "테스트유저3",
-    usertype: "0",
-    email: "testuser3@naver.com",
-    phone_number: "010-1234-5678",
-    weight: "70",
-    age: "23",
-    last_accessed_at: "2021-10-01T14:46:00.000Z",
-    total_money: "100000",
-  },
+    "id": 1,
+    "password": "abcd1234",
+    "username": "김데베", 
+    "user_type": 1,
+    "email": "kw_db@kw.ac.kr",
+    "phone_number": "1.012345678E9",
+    "weight": 50.0,
+    "age": 20,
+    "last_accessed_at": null,
+    "total_money": 50000
+  }
 ];
 
 const AdminUser = () => {
   const [userData, setUserData] = useState(mock);
   useEffect(() => {
     // TODO: Fetch data
+    (async() => {
+      try {
+        const response = await fetch(process.env.REACT_APP_API_URL + "/admin/get-all-users", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        });
+        if (response.status !== 200) {
+          throw new Error("데이터를 가져오는데 실패하였습니다.");
+        }
+        const jsonData = await response.json();
+        console.log(jsonData.result)
+        setUserData(jsonData.result)
+      } catch (error) {
+        alert("데이터를 가져오는데 실패하였습니다.");
+      } 
+    })();
   }, []);
   return (
     <TableContainer component={Paper}>
-      <TableHead>
+      <TableHead sx={{width: "100%"}}>
         <TableRow>
-          <TableCell className="tablecell">
+          <TableCell className="user-table-cell">
             <Typography variant="span">번호</Typography>
           </TableCell>
-          <TableCell className="tablecell">
-            <Typography variant="span">아이디</Typography>
-          </TableCell>
-          <TableCell className="tablecell">
+          <TableCell className="user-table-cell">
             <Typography variant="span">이메일</Typography>
           </TableCell>
-          <TableCell className="tablecell">
+          <TableCell className="user-table-cell">
             <Typography variant="span">이름</Typography>
           </TableCell>
-          <TableCell className="tablecell">
+          <TableCell className="user-table-cell">
             <Typography variant="span">전화번호</Typography>
           </TableCell>
-          <TableCell className="tablecell">
+          <TableCell className="user-table-cell">
             <Typography variant="span">무게</Typography>
           </TableCell>
-          <TableCell className="tablecell">
+          <TableCell className="user-table-cell">
             <Typography variant="span">나이</Typography>
           </TableCell>
-          <TableCell className="tablecell">
+          <TableCell className="user-table-cell">
             <Typography variant="span">마지막 접속시간</Typography>
           </TableCell>
-          <TableCell className="tablecell">
+          <TableCell className="user-table-cell">
             <Typography variant="span">보유금액</Typography>
+          </TableCell>
+          <TableCell className="user-table-cell">
+            <Typography variant="span">수정</Typography>
           </TableCell>
         </TableRow>
       </TableHead>
-      <TableBody>
+      <TableBody sx={{width: "100%"}}>
         {userData && userData.length > 0 ? (
           userData.map((row, index) => (
             <TableRow>
-              <TableCell className="tablecell">
-                <Typography variant="span">{index + 1}</Typography>
+              <TableCell className="user-table-cell">
+                <Typography variant="span">{row.id}</Typography>
               </TableCell>
-              <TableCell className="tablecell">
-                <Typography variant="span">{row.userid}</Typography>
-              </TableCell>
-              <TableCell className="tablecell">
+              <TableCell className="user-table-cell">
                 <Typography variant="span">{row.email}</Typography>
               </TableCell>
-              <TableCell className="tablecell">
+              <TableCell className="user-table-cell">
                 <Typography variant="span">{row.username}</Typography>
               </TableCell>
-              <TableCell className="tablecell">
+              <TableCell className="user-table-cell">
                 <Typography variant="span">{row.phone_number}</Typography>
               </TableCell>
-              <TableCell className="tablecell">
+              <TableCell className="user-table-cell">
                 <Typography variant="span">{row.weight}</Typography>
               </TableCell>
-              <TableCell className="tablecell">
+              <TableCell className="user-table-cell">
                 <Typography variant="span">{row.age}</Typography>
               </TableCell>
-              <TableCell className="tablecell">
+              <TableCell className="user-table-cell">
                 <Typography variant="span">{moment(row.last_accessed_at).format("YYYY-MM-DD HH:mm:ss")}</Typography>
               </TableCell>
-              <TableCell className="tablecell">
+              <TableCell className="user-table-cell">
                 <Typography variant="span">{row.total_money}</Typography>
+              </TableCell>
+              <TableCell className="user-table-cell">
+                <Button variant="contained" component={RouterLink} to={`/admin/user/edit?id=${row.id}`}>
+                  수정
+                </Button>
               </TableCell>
             </TableRow>
           ))
