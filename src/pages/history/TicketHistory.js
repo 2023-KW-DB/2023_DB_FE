cbbd9d0053589c2005c66380770a65e8c70c95dc
김성진 <mirusu400@naver.com>
2023-11-29 06:12:42 +0900

    fix: History에 pagination추가

--- src/pages/history/TicketHistory.js
+++ src/pages/history/TicketHistory.js
@@ -1,10 +1,13 @@
-import { TableContainer, Table, TableHead, TableCell, TableBody, TableRow, Paper } from "@mui/material";
+import { TableContainer, Table, TableHead, TableCell, TableBody, TableRow, Paper, Box, ButtonGroup, Button } from "@mui/material";
 import moment from "moment";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 const TicketHistory = () => {
   const [ticketHistory, setTicketHIstory] = useState([]);
+  const [showData, setShowData] = useState([]);
+  const [page, setPages] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
   const user = useSelector((state) => state.user);
   useEffect(() => {
     (async() => {
@@ -18,43 +21,58 @@ const TicketHistory = () => {
         }
         const jsonData = await response.json();
         setTicketHIstory(jsonData.result);
+        setTotalPage(parseInt(jsonData.result.length / 10) + 1)
+        setShowData(jsonData.result.slice(0, 10))
       } catch (e) {
         alert("데이터를 가져오는데 실패하였습니다.");
       }
     })();
   }, [])
+
+  useEffect(() => {
+    setShowData(ticketHistory.slice((page - 1) * 10, page * 10));
+  }, [page]);
   
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="Usage History">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">
-              티켓 번호
-            </TableCell>
-            <TableCell align="center">
-              구매 시간
-            </TableCell>
-            <TableCell align="center">
-              사용 여부
-            </TableCell>
-          </TableRow>
-        </TableHead>
-
-        <TableBody>
-          {ticketHistory.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell align="center">{row.ticket_id}</TableCell>
-              <TableCell align="center">{moment(row.purchased_at).format("YYYY-MM-DD HH:mm:ss")}</TableCell>
-              <TableCell align="center">{row.is_used ? "사용 완료" : "사용 전"}</TableCell>
+    <>
+      <TableContainer component={Paper}>
+        <Table aria-label="Usage History">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">
+                티켓 번호
+              </TableCell>
+              <TableCell align="center">
+                구매 시간
+              </TableCell>
+              <TableCell align="center">
+                사용 여부
+              </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+
+          <TableBody>
+            {ticketHistory.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="center">{row.ticket_id}</TableCell>
+                <TableCell align="center">{moment(row.purchased_at).format("YYYY-MM-DD HH:mm:ss")}</TableCell>
+                <TableCell align="center">{row.is_used ? "사용 완료" : "사용 전"}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Box sx={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", width: "100%", py: 3}}>
+        <ButtonGroup>
+          <Button onClick={() => setPages(page - 1 > 0 ? page - 1 : 1)}>이전</Button>
+          <Button>{page}</Button>
+          <Button onClick={() => setPages(page + 1 < totalPage ? page + 1 : totalPage)}>다음</Button>
+        </ButtonGroup>
+      </Box>
+    </>
   )
 }
 
--- src/pages/history/TravelHistory.js
+++ src/pages/history/TravelHistory.js
@@ -1,4 +1,4 @@
-import { TableContainer, Table, TableHead, TableCell, TableBody, TableRow, Paper } from "@mui/material";
+import { TableContainer, Table, TableHead, TableCell, TableBody, TableRow, Paper, Box, ButtonGroup, Button } from "@mui/material";
 import moment from "moment";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
@@ -6,6 +6,9 @@ import { useSelector } from "react-redux";
 
 const TravelHistory = () => {
   const [travelHistory, setTravelHistory] = useState([]);
+  const [showData, setShowData] = useState([]);
+  const [page, setPages] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
   const user = useSelector((state) => state.user);
   useEffect(() => {
     (async() => {
@@ -19,51 +22,66 @@ const TravelHistory = () => {
         }
         const jsonData = await response.json();
         setTravelHistory(jsonData.result);
+        setTotalPage(parseInt(jsonData.result.length / 10) + 1)
+        setShowData(jsonData.result.slice(0, 10))
       } catch (e) {
         alert("데이터를 가져오는데 실패하였습니다.");
       }
     })();
   }, [])
 
-  return (
-    <TableContainer component={Paper}>
-      <Table aria-label="Usage History">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">
-              출발지
-            </TableCell>
-            <TableCell align="center">
-              출발시간
-            </TableCell>
-            <TableCell align="center">
-              도착지
-            </TableCell>
-            <TableCell align="center">
-              도착시간
-            </TableCell>
-            <TableCell sx={{minWidth: 100}} align="center">
-              이동 거리
-            </TableCell>
-          </TableRow>
-        </TableHead>
+  useEffect(() => {
+    setShowData(travelHistory.slice((page - 1) * 10, page * 10));
+  }, [page]);
 
-        <TableBody>
-          {travelHistory.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell align="center">{row.departure_station}</TableCell>
-              <TableCell align="center">{moment(row.departure_time).format("YYYY-MM-DD HH:mm:ss")}</TableCell>
-              <TableCell align="center">{row.arrival_station}</TableCell>
-              <TableCell align="center">{moment(row.arrival_time).format("YYYY-MM-DD HH:mm:ss")}</TableCell>
-              <TableCell align="center">{row.use_distance}m</TableCell>
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table aria-label="Usage History">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">
+                출발지
+              </TableCell>
+              <TableCell align="center">
+                출발시간
+              </TableCell>
+              <TableCell align="center">
+                도착지
+              </TableCell>
+              <TableCell align="center">
+                도착시간
+              </TableCell>
+              <TableCell sx={{minWidth: 100}} align="center">
+                이동 거리
+              </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+
+          <TableBody>
+            {showData.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="center">{row.departure_station}</TableCell>
+                <TableCell align="center">{moment(row.departure_time).format("YYYY-MM-DD HH:mm:ss")}</TableCell>
+                <TableCell align="center">{row.arrival_station}</TableCell>
+                <TableCell align="center">{moment(row.arrival_time).format("YYYY-MM-DD HH:mm:ss")}</TableCell>
+                <TableCell align="center">{row.use_distance}m</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Box sx={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", width: "100%", py: 3}}>
+        <ButtonGroup>
+          <Button onClick={() => setPages(page - 1 > 0 ? page - 1 : 1)}>이전</Button>
+          <Button>{page}</Button>
+          <Button onClick={() => setPages(page + 1 < totalPage ? page + 1 : totalPage)}>다음</Button>
+        </ButtonGroup>
+      </Box>
+    </>
   )
 }
 
