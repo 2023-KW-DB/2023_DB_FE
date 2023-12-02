ab6030a3ea7b042a61e510cb68f56a2635521cd6
mirusu400 <mirusu400@naver.com>
2023-12-02 19:02:45 +0900

    fix: 로그아웃 시 쿠키 Expire 로직 추가

--- src/Layout.js
+++ src/Layout.js
@@ -133,7 +133,14 @@ function DrawerAppBar(props) {
         console.log(error);
       }
       // Remove all cookie
-      document.cookie = "";
+      const cookies = document.cookie.split(";");
+
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+      }
       dispatch(setUser({}));
       document.location.href = "/";
     })();
