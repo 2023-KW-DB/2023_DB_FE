ad55cb5e9aab64cdb69ae66ca60b4fafae1f8f49
김성진 <mirusu400@naver.com>
2023-12-02 14:42:32 +0900

    fix: Ticket 구매 시 현재 가격 표시

--- src/pages/BuyTicket.js
+++ src/pages/BuyTicket.js
@@ -1,21 +1,8 @@
-import {
-  Box,
-  Button,
-  Container,
-  CssBaseline,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Container, CssBaseline, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { ConfirmationNumber, Redeem } from "@mui/icons-material";
 import { useSelector } from "react-redux";
-
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
 const mock = [
   {
     id: 1,
@@ -30,17 +17,15 @@ const mock = [
 const BuyTicket = () => {
   const [ticketList, setTicketList] = useState(mock);
   const user = useSelector((state) => state.user);
+  const [money, setMoney] = useState(0);
 
   useEffect(() => {
     (async () => {
       try {
-        const response = await fetch(
-          process.env.REACT_APP_API_URL + "/get-all-ticket",
-          {
-            method: "GET",
-            headers: { "Content-Type": "application/json" },
-          },
-        );
+        const response = await fetch(process.env.REACT_APP_API_URL + "/get-all-ticket", {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        });
         if (response.status !== 200) {
           throw new Error("데이터를 가져오는데 실패하였습니다.");
         }
@@ -52,22 +37,28 @@ const BuyTicket = () => {
     })();
   }, []);
 
+  useEffect(() => {
+    setMoney(user.total_money);
+  }, [user]);
+
   const buyTicketHandler = (id) => {
     (async () => {
       try {
-        const response = await fetch(
-          process.env.REACT_APP_API_URL +
-            `/users/purchase-ticket?userId=${user.id}&ticketId=${id}`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-          },
-        );
+        const response = await fetch(process.env.REACT_APP_API_URL + `/users/purchase-ticket?userId=${user.id}&ticketId=${id}`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+        });
         if (response.status !== 200) {
           throw new Error("티켓 구매에 실패했습니다.");
         }
         const jsonData = await response.json();
         alert("티켓 구매에 성공했습니다.");
+        for (let i = 0; i < ticketList.length; i++) {
+          if (ticketList[i].id === id) {
+            setMoney(money - ticketList[i].ticket_price);
+            break;
+          }
+        }
       } catch (e) {
         alert("티켓 구매에 실패했습니다.");
       }
@@ -80,6 +71,9 @@ const BuyTicket = () => {
       <Typography component="h1" variant="h5" sx={{ py: 3 }}>
         이용권 구매
       </Typography>
+      <Box sx={{ display: "flex", py: 1 }}>
+        <AttachMoneyIcon sx={{ mr: 1 }} /> 보유 금액 : {money}&nbsp;원
+      </Box>
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
@@ -109,9 +103,7 @@ const BuyTicket = () => {
                     {/* <TableCell>
                       <Typography variant="span">{row.id}</Typography>
                     </TableCell> */}
-                    <TableCell
-                      sx={{ alignItems: "center", textAlign: "center" }}
-                    >
+                    <TableCell sx={{ alignItems: "center", textAlign: "center" }}>
                       <Typography variant="span">{row.ticket_price}</Typography>
                     </TableCell>
                     <TableCell
@@ -121,11 +113,7 @@ const BuyTicket = () => {
                         textAlign: "center",
                       }}
                     >
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={() => buyTicketHandler(row.id)}
-                      >
+                      <Button variant="contained" color="primary" onClick={() => buyTicketHandler(row.id)}>
                         <Redeem /> 구매하기
                       </Button>
                     </TableCell>
